ec8324ea40400fd1999474895e4135b3363df2c1
Aidan Killeen <aidan.killeen@gmail.com>
2021-04-12 12:44:41 +0100

    added in ssl cert and redirect to https

--- nodejshw/index.js
+++ nodejshw/index.js
@@ -1,16 +1,39 @@
 // node.js express web server
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
 const express = require('express');
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => { 
-    res.send(`
-    <h1>Node.js Web Server</h1>
-    <p>this is working.</p>`);
+const credentials = {
+    key: fs.readFileSync('./security/cert.key', 'utf8'), 
+    cert: fs.readFileSync('./security/cert.pem', 'utf8')
+};
+
+app.use((req, res, next)=> {
+    console.log("middleware called");
+    if (req.secure) {
+        next();
+    } else {
+        //res.send("use https!");
+        let url = `https://${req.headers.host}${req.url}`;
+        console.log(`redirecting to ${url}`);
+        res.redirect(url);
+    }
 });
 
-app.listen(port, ()=>{
-    console.log(`Web server listening on port ${port}`);
+app.use(express.static('public'));
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, ()=>{
+    console.log(`HTTP Web server listening on port 80`);
+});
+
+httpsServer.listen(443, ()=> {
+    console.log(`HTTPS Web server listening on port 443`);
 });
 
 

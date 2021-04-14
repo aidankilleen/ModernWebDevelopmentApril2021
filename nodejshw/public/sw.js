d98b0f1a5c1a66c2e8ed95caa45d237b74baa99a
Aidan Killeen <aidan.killeen@gmail.com>
2021-04-14 13:02:01 +0100

    added caching to the service worker

--- nodejshw/public/sw.js
+++ nodejshw/public/sw.js
@@ -1,2 +1,28 @@
 // Service Worker for ModernWebDev PWA
 
+let filesToCache = [
+    '/', 
+    '/index.html', 
+    '/css/styles.css', 
+    '/js/pwascripts.js'
+];
+
+let cacheId = "modernwebpwa";
+
+self.addEventListener('install', function(e) {
+    console.log("install triggered");
+
+    e.waitUntil(
+        caches.open(cacheId)
+            .then(function(cache) {
+                return cache.addAll(filesToCache);
+            })
+    );
+});
+
+/*
+self.addEventListener('fetch', function(e) {
+    console.log("fetch called");
+});
+*/
+

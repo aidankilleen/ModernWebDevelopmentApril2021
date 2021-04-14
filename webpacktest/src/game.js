d7e4e273d5250605e5f587f0dbd6c664204c7f4a
Aidan Killeen <aidan.killeen@gmail.com>
2021-04-14 15:36:09 +0100

    added webpack demo

---
+++ webpacktest/src/game.js
@@ -0,0 +1,18 @@
+let numTimesClicked = 0;
+
+function win() {
+    alert("You Win!");
+    reset();
+}
+
+function reset() {
+    numTimesClicked = 0;
+}
+
+function click() {
+    numTimesClicked++;
+    console.log(`clicked:${numTimesClicked}`);
+    if (numTimesClicked == 10) {
+        win();
+    }
+}

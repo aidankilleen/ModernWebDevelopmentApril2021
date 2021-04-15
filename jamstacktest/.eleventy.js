0484d47b6e133d4ec0f8569008867e12df58c2c3
Aidan Killeen <aidan.killeen@gmail.com>
2021-04-15 15:02:27 +0100

    added hero section type to homepage

--- jamstacktest/.eleventy.js
+++ jamstacktest/.eleventy.js
@@ -1,13 +1,25 @@
 module.exports = function(config) {
 
     config.addPassthroughCopy({'./src/styles/styles.css': 'styles/styles.css'});
+    config.addPassthroughCopy({'./src/images/': 'images'});
 
     config.addFilter('makeSection', function(section) {
 
-        return `<section class="section" id="${section.id}">
-                    <h1>${ section.title}</h1>
-                    <p>${ section.text }</p>
-                </section>`;
+        if (section.type == "hero") {
+            return `<section class="section" 
+                        id="${section.id}" 
+                        style="background-image:url(${section.image});background-size:contain;">
+                <h1>${ section.title}</h1>
+                <p>${ section.text }</p>
+            </section>`;
+
+        } else {
+            return `<section class="section" id="${section.id}">
+                <h1>${ section.title}</h1>
+                <p>${ section.text }</p>
+            </section>`;
+
+        }
     });
 
     return {
